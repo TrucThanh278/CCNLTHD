571d3b82b72d14b2bb1cbc10f1485b74c0c32f9b
TrucThanh <trucnguyenthanh56@gmail.com>
2024-03-07 11:23:18 +0700

    init project

---
+++ CCNLTHD/course/admin.py
@@ -0,0 +1,33 @@
+from django.contrib import admin
+from course.models import Category, Course
+from django.utils.html import mark_safe
+from django import forms
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+
+
+class CourseForm(forms.ModelForm):
+    description = forms.CharField(widget=CKEditorUploadingWidget)
+    class Meta:
+        model = Course
+        fields = '__all__'
+
+
+class CourseAmdin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'created_date', 'updated_date', 'active']
+    search_fields = ['name', 'description']
+    list_filter = ['id', 'name']
+    readonly_fields = ['my_image']
+    form = CourseForm
+
+    def my_image(self, course):
+        if course.image:
+            return mark_safe(f"<img width='200' src='{course.image.url}' />")
+
+    class Media:
+        css = {
+            'all': ['/static/css/style.css']
+        }
+
+# Register your models here.
+admin.site.register(Category)
+admin.site.register(Course, CourseAmdin)
